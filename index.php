36d40df97c61f93b8ef7aab13c0c63e51681dbef
Gary Hepting <engineering+gary@sidereel.com>
2013-10-07 08:38:41 -0700

    updated resizer to use groundwork 2.3.x

--- index.php
+++ index.php
@@ -7,31 +7,31 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-  <title>Viewport Resizer for Testing Responsive Web Design</title>
+  <title><?= str_replace('http://', '', $_REQUEST['url']) ?></title>
   <!-- Modernizr -->
-  <script src="http://groundwork.sidereel.com/groundwork/js/libs/modernizr-2.6.2.min.js"></script>
-  <!-- jQuery -->
-  <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-  <script type="text/javascript">window.jQuery || document.write('<script src="http://groundwork.sidereel.com/groundwork/js/libs/jquery-1.8.2.min.js"><\/script>')</script>
-  <!-- Groundwork CSS -->
-  <link type="text/css" rel="stylesheet" href="http://groundwork.sidereel.com/groundwork/css/groundwork.css">
-  <!--[if IE 7]>
-  <link type="text/css" rel="stylesheet" href="http://groundwork.sidereel.com/groundwork/css/font-awesome-ie7.min.css">
+  <script src="http://groundworkcss.github.io/js/libs/modernizr-2.6.2.min.js"></script>
+  <!-- jQuery-->
+  <script type="text/javascript" src="http://groundworkcss.github.io/js/libs/jquery-1.10.2.min.js"></script>
+  <!-- framework css -->
+  <!--[if gt IE 9]><!-->
+  <link type="text/css" rel="stylesheet" href="http://groundworkcss.github.io/css/groundwork.css">
+  <!--<![endif]-->
+  <!--[if lte IE 9]>
+  <link type="text/css" rel="stylesheet" href="http://groundworkcss.github.io/css/groundwork-core.css">
+  <link type="text/css" rel="stylesheet" href="http://groundworkcss.github.io/css/groundwork-type.css">
+  <link type="text/css" rel="stylesheet" href="http://groundworkcss.github.io/css/groundwork-ui.css">
+  <link type="text/css" rel="stylesheet" href="http://groundworkcss.github.io/css/groundwork-anim.css">
+  <link type="text/css" rel="stylesheet" href="http://groundworkcss.github.io/css/groundwork-ie.css">
   <![endif]-->
 </head>
-<body>
-
   <style type="text/css">
     html, body {
       width:100%;
       height:100%;
       text-align:center;
-      background:#333 url('http://groundwork.sidereel.com/images/black_linen_v2.png') center repeat;
+      background:#333 url('http://groundworkcss.github.io/images/black_linen_v2.png') center repeat;
       overflow:hidden;
       color:#fff;
-    }
-    .container {
-      max-width:480px;
     }
     #resizerFrame {
       width:100%;
@@ -41,42 +41,45 @@
       margin:0 auto;
       background:white;
       box-shadow:0 0 50px #000;
+      -webkit-transition: max-width 0.35s ease-out, max-height 0.35s ease-out;
+      -moz-transition: max-width 0.35s ease-out, max-height 0.35s ease-out;
+      -ms-transition: max-width 0.35s ease-out, max-height 0.35s ease-out;
+      -o-transition: max-width 0.35s ease-out, max-height 0.35s ease-out;
+      transition: max-width 0.35s ease-out, max-height 0.35s ease-out;
     }
     #resizer {
       position:fixed;
       top:0;
+      left:0;
+      right:0;
       z-index:99;
-      width:450px;
-      left:50%;
-      height:2em;
+      width:100%;
+      text-align:center;
       margin:0 auto;
-      margin-left:-225px;
     }
     #resizer ul {
+      font-size: 17px;
+      display: inline-block;
       -webkit-filter: drop-shadow(0 1px 5px rgba(0,0,0,.25));
       -moz-filter: drop-shadow(0 1px 5px rgba(0,0,0,.25));
       -ms-filter: drop-shadow(0 1px 5px rgba(0,0,0,.25));
       -o-filter: drop-shadow(0 1px 5px rgba(0,0,0,.25));
       filter: drop-shadow(0 1px 5px rgba(0,0,0,.25));
+      -ms-transform: translateZ(0);
+      -o-transform: translateZ(0);
+      -moz-transform: translateZ(0);
+      -webkit-transform: translateZ(0);
+      transform: translateZ(0);
       margin:-0.2em auto 0;
     }
-    #closeResizer, #openHome {
-      padding:0.3em 0.5em;
-      background:#fff;
-      border-radius:0.3em;
-      margin:1px 2px;
-      text-decoration:none;
-      color:#222;
-      box-shadow:0 0 5px rgba(0,0,0,0.35);
-    }
-    a.rotate i, a[data-rotate] i {
+    button.rotate i:before, button[data-rotate] i:before {
       -webkit-transition: all 0.15s linear;
       -moz-transition: all 0.15s linear;
-      -mx-transition: all 0.15s linear;
+      -ms-transition: all 0.15s linear;
       -o-transition: all 0.15s linear;
       transition: all 0.15s linear;
     }
-    .rotate-90-ctr    {
+    .landscape i:before {
       -webkit-transform: rotate(-90deg);
       -moz-transform: rotate(-90deg);
       -ms-transform: rotate(-90deg);
@@ -84,6 +87,87 @@
       transform: rotate(-90deg);
     }
   </style>
+  <script type="text/javascript">
+    function querystring(key) {
+      var re=new RegExp('(?:\\?|&)'+key+'=(.*?)(?=&|$)','gi');
+      var r=[], m;
+      while ((m=re.exec(document.location.search)) != null) r.push(m[1]);
+      return r;
+    }
+    function basename(str) {
+      var base = new String(str).substring(str.lastIndexOf('/') + 1);
+      if(base.lastIndexOf(".") != -1) {
+        base = base.substring(0, base.lastIndexOf("."));
+      }
+      return base;
+    }
+    function updateQueryStringParameter(uri, key, value) {
+      var re = new RegExp("([?|&])" + key + "=.*?(&|$)", "i");
+      separator = uri.indexOf('?') !== -1 ? "&" : "?";
+      var url = window.location.href;
+      if (uri.match(re)) {
+        url = uri.replace(re, '$1' + key + "=" + value + '$2');
+      }
+      else {
+        url = uri + separator + key + "=" + value;
+      }
+      return url;
+    }
+    $(document).ready(function() {
+      $('button[data-viewport-width]').on('click', function(e) {
+        if($(this).attr('data-viewport-width') == '100%') {
+          newWidth = '100%';
+        }else{
+          newWidth = $(this).attr('data-viewport-width');
+        }
+        if($(this).attr('data-viewport-height') == '100%') {
+          newHeight = '100%';
+        }else{
+          newHeight = $(this).attr('data-viewport-height');
+        }
+        $('button[data-viewport-width]').removeClass('active');
+        $(this).addClass('active');
+        // $('#resizerFrame').stop().animate({'max-width': newWidth, 'max-height': newHeight}, 200);
+        $('#resizerFrame').css({'max-width': newWidth, 'max-height': newHeight});
+        e.preventDefault();
+        return false;
+      });
+      $('button.rotate').on('click', function(e) {
+        $(this).toggleClass('landscape');
+        $('button[data-rotate=true]').each(function() {
+          $(this).toggleClass('landscape');
+          width = $(this).attr('data-viewport-width');
+          height = $(this).attr('data-viewport-height');
+          // shuffle values
+          $(this).attr('data-viewport-width', height);
+          $(this).attr('data-viewport-height', width);
+          if($(this).hasClass('active')) {
+            $(this).trigger('click');
+          }
+        });
+      });
+      $('#closeResizer').on('click', function(e) {
+        newWidth = $(window).width();
+        newHeight = $(window).height();
+        $('button[data-viewport-width]').removeClass('active');
+        // $('#resizerFrame').stop().animate({'max-width': newWidth, 'max-height': newHeight}, 200);
+        $('#resizerFrame').css({'max-width': newWidth, 'max-height': newHeight});
+        $('#resizer').fadeOut(500, function() {
+          document.location = document.getElementById("resizerFrame").contentWindow.location.href;
+        });
+        e.preventDefault();
+        return false;
+      });
+      if(querystring('url').length > 0) {
+        $('#resizerFrame').attr('src',querystring('url'));
+      }
+      $('#resizerFrame').on('load', function() {
+        var url = basename(this.contentWindow.location.href);
+        var stateObj = { resizer: 'resizer' };
+        history.pushState(stateObj, url, href);
+      });
+    });
+  </script>
 <?php
 if(!isset($_REQUEST['url']) || empty($_REQUEST['url'])) {
 ?>
@@ -123,137 +207,27 @@ if(!isset($_REQUEST['url']) || empty($_REQUEST['url'])) {
     $url = "http://" . $url;
   }
 ?>
-  <script type="text/javascript">
-
-    // function to update query string params
-    function updateQueryStringParameter(uri, key, value) {
-      var re = new RegExp("([?|&])" + key + "=.*?(&|$)", "i");
-      separator = uri.indexOf('?') !== -1 ? "&" : "?";
-      var url = window.location.href;
-      if (uri.match(re)) {
-        url = uri.replace(re, '$1' + key + "=" + value + '$2');
-      }
-      else {
-        url = uri + separator + key + "=" + value;
-      }
-      return url;
-    }
-
-    var stateObj = { resizer: 'resizer' };
-
-    $(document).ready(function() {
-
-      // set maximum dimensions of display frame
-      $('#resizerFrame').each(function() {
-        $(this).css('max-width',$(window).width());
-        $(this).css('max-height',$(window).height());
-      });
-
-      // change display frame size
-      $('a[data-viewport-width]').on('click', function(e) {
-        if($(this).attr('data-viewport-width') == '100%') {
-          newWidth = '100%';
-        }else{
-          newWidth = $(this).attr('data-viewport-width');
-        }
-        if($(this).attr('data-viewport-height') == '100%') {
-          newHeight = '100%';
-        }else{
-          newHeight = $(this).attr('data-viewport-height');
-        }
-        $('a[data-viewport-width]').removeClass('active');
-        $(this).addClass('active');
-        $('#resizerFrame').stop().animate({'max-width': newWidth, 'max-height': newHeight}, 200);
-        $('title').text('W: '+newWidth+', H: '+newHeight);
-        var size = $('#resizer').find('a.active').attr('data-title');
-        var href = updateQueryStringParameter(document.location.href, 'size', size);
-        history.pushState(stateObj, document.location.href, href);
-        e.preventDefault();
-        return false;
-      });
-
-      // rotate display frame
-      $('a.rotate').on('click', function(e) {
-        $(this).children('i[class*=icon]').toggleClass('rotate-90-ctr');
-        $('a[data-rotate=true]').each(function() {
-          $(this).children('i[class*=icon]').toggleClass('rotate-90-ctr');
-          width = $(this).attr('data-viewport-width');
-          height = $(this).attr('data-viewport-height');
-          // shuffle values
-          $(this).attr('data-viewport-width', height);
-          $(this).attr('data-viewport-height', width);
-          if($(this).hasClass('active')) {
-            $(this).trigger('click');
-          }
-        });
-        var orientation = $('#resizer').find('.rotate-90-ctr').length > 0 ? 'Landscape' : 'Portrait';
-        var href = updateQueryStringParameter(document.location.href, 'orientation', orientation);
-        history.pushState(stateObj, document.location.href, href);
-        e.preventDefault();
-        return false;
-      });
-
-      // set display frame size on page load
-      $('a[data-viewport-width].active').trigger('click');
-<?php
-if($_REQUEST['orientation'] == 'Landscape') {
-?>
-      // set display frame orientation
-      $('a.rotate').trigger('click');
-<?php
-}
-?>
-
-      // close resizer toolbar
-      $('#closeResizer').on('click', function(e) {
-        newWidth = $(window).width();
-        newHeight = $(window).height();
-        $('a[data-viewport-width]').removeClass('active');
-        $('#resizerFrame').stop().animate({'max-width': newWidth, 'max-height': newHeight}, 200);
-        $('#resizer').fadeOut(500, function() {
-          document.location = document.getElementById("resizerFrame").contentWindow.location.href || $("#resizerFrame").attr('src');
-        });
-        e.preventDefault();
-        return false;
-      });
-
-      // set display frame source and update url params
-      $('#resizerFrame').on('load', function() {
-        var url = $(this).attr('src');
-        if(!url.toLowerCase().match('^https?://')) {
-          url = "http://" + url;
-        }
-        var href = updateQueryStringParameter(document.location.href, 'url', url);
-        var size = $('#resizer').find('a.active').attr('data-title');
-        href = updateQueryStringParameter(document.location.href, 'size', size);
-        history.pushState(stateObj, url, href);
-      });
-
-    });
-
-  </script>
-  <div id="resizer">
-    <a id="openHome" class="pull-left" title="Resizer Home" href="./"><i class="icon-home"></i></a>
-    <ul class="button-list pull-left">
-      <li><a class="tooltip <?php echo (empty($_REQUEST['size']) || $_REQUEST['size'] == 'Fullscreen' ? 'active' : '') ?>" title="Fullscreen" href="#" data-viewport-width="100%" data-viewport-height="100%"><i class="icon-fullscreen"></i></a></li>
-      <li><a class="tooltip <?php echo $_REQUEST['size'] == 'Desktop' ? 'active' : '' ?>" title="Desktop" href="#" data-viewport-width="1920" data-viewport-height="1080"><i class="icon-desktop"></i></a></li>
-      <li><a class="tooltip <?php echo $_REQUEST['size'] == 'MacBook' ? 'active' : '' ?>" title="MacBook" href="#" data-viewport-width="1280px" data-viewport-height="800px"><i class="icon-laptop"></i></a></li>
-      <li><a class="tooltip <?php echo $_REQUEST['size'] == 'iPad' ? 'active' : '' ?>" title="iPad" href="#" data-rotate="true" data-viewport-width="768px" data-viewport-height="1024px"><i class="icon-tablet"></i></a></li>
-      <li><a class="tooltip <?php echo $_REQUEST['size'] == 'Android Tablet' ? 'active' : '' ?>" title="Android Tablet" href="#" data-rotate="true" data-viewport-width="720px" data-viewport-height="1280px"><i class="icon-tablet" style="font-size:0.9em;"></i></a></li>
-      <li><a class="tooltip <?php echo $_REQUEST['size'] == 'Android Phone' ? 'active' : '' ?>" title="Android Phone" href="#" data-rotate="true" data-viewport-width="480px" data-viewport-height="720px"><i class="icon-mobile-phone"></i></a></li>
-      <li><a class="tooltip <?php echo $_REQUEST['size'] == 'iPhone' ? 'active' : '' ?>" title="iPhone" href="#" data-rotate="true" data-viewport-width="320px" data-viewport-height="640px"><i class="icon-mobile-phone" style="font-size:0.9em;"></i></a></li>
-      <li><a class="tooltip rotate <?php echo $_REQUEST['orientation'] == 'landscape' ? 'rotate-90-ctr' : '' ?>" title="Toggle Landscape/Portrait" href="#"><i class="icon-refresh"></i></a></li>
-    </ul>
-    <a id="closeResizer" class="pull-left" title="Close Resizer" href="./"><i class="icon-remove"></i></a>
+  <div style="position:fixed;top:0;z-index:99;width:100%;">
+    <div id="resizer" class="bounceInDown animated">
+      <ul class="button-group">
+        <li><button class="desktop-only" title="Fullscreen" data-viewport-width="100%" data-viewport-height="100%"><i class="icon-fullscreen"></i></button></li>
+        <li><button class="desktop-only" title="Desktop" data-viewport-width="1920px" data-viewport-height="1080px"><i class="icon-desktop"></i></button></li>
+        <li><button class="desktop-only" title="13 &#39; MacBook Pro" data-viewport-width="1280px" data-viewport-height="800px"><i class="icon-laptop"></i></button></li>
+        <li><button class="hide-on-small-tablet hide-on-mobile" title="iPad" data-rotate="true" data-viewport-width="768px" data-viewport-height="1024px"><i class="icon-tablet"></i></button></li>
+        <li><button class="hide-on-mobile" title="Android Tablet" data-rotate="true" data-viewport-width="720px" data-viewport-height="1280px"><i class="android icon-tablet"></i></button></li>
+        <li><button class="" title="Android Phone" data-rotate="true" data-viewport-width="480px" data-viewport-height="720px"><i class="android icon-mobile-phone"></i></button></li>
+        <li><button class="" title="iPhone" data-rotate="true" data-viewport-width="320px" data-viewport-height="640px"><i class="icon-mobile-phone"></i></button></li>
+        <li><button class="rotate" title="Toggle Landscape/Portrait"><i class="icon-refresh"></i></button></li>
+        <li><button id="closeResizer"><i class="icon-remove"></i></button></li>
+      </ul>
+    </div>
   </div>
   <iframe id="resizerFrame" src="<?php echo $url ?>" frameborder="0"></iframe>
 <?php
 }
 ?>
   <!-- scripts -->
-  <script type="text/javascript" src="http://groundwork.sidereel.com/groundwork/js/plugins/jquery.cycle2.js"></script>
-  <script type="text/javascript" src="http://groundwork.sidereel.com/groundwork/js/plugins/jquery.magnific-popup.js"></script>
-  <script type="text/javascript" src="http://groundwork.sidereel.com/groundwork/js/groundwork.all.js"></script>
+  <script type="text/javascript" src="http://groundworkcss.github.io/js/groundwork.all.js"></script>
 
   <!-- google analytics -->
   <script type="text/javascript">
