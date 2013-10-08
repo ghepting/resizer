d5ddcb1979e626019ecb6876b2cfb02c4b7c2a3f
Gary Hepting <engineering+gary@sidereel.com>
2013-10-08 16:09:59 -0700

    added conditional display of refresh/close buttons due to security rescrictions related to these functions

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+function root_url() {
+  $s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
+  $sp = strtolower($_SERVER["SERVER_PROTOCOL"]);
+  $protocol = substr($sp, 0, strpos($sp, "/")) . $s;
+  $port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]);
+  return $protocol . "://" . $_SERVER['SERVER_NAME'] . $port;
+}
+
+function iframe_url() {
+  $url = $_REQUEST['url'];
+  $url = parse_url($url);
+  return $url['scheme'] . "://" . $url['host'] . ($url['port'] ? ":" . $url['port'] : "");
+}
+
+$iframe_access = (root_url() == iframe_url());
+?>
 <!doctype html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -52,9 +69,9 @@
       line-height: 1;
     }
     #resizer ul {
-      font-size: 17px;
+      font-size: 13px;
       display: inline-block;
-      margin: -0.2em auto 0;
+      margin: -0.2em auto 0.2em;
       -webkit-filter: drop-shadow(0 1px 5px rgba(0,0,0,.25));
       -moz-filter: drop-shadow(0 1px 5px rgba(0,0,0,.25));
       -ms-filter: drop-shadow(0 1px 5px rgba(0,0,0,.25));
@@ -82,6 +99,31 @@
     }
   </style>
   <script type="text/javascript">
+    function setCookie(name,value,days) {
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+      }
+      else var expires = "";
+      document.cookie = name+"="+value+expires+"; path=/";
+    }
+
+    function readCookie(name) {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+      }
+      return null;
+    }
+
+    function eraseCookie(name) {
+      setCookie(name,"",-1);
+    }
+
     function basename(str) {
       var base = new String(str).substring(str.lastIndexOf('/') + 1);
       if(base.lastIndexOf(".") != -1) {
@@ -89,6 +131,7 @@
       }
       return base;
     }
+
     function updateQueryStringParameter(uri, key, value) {
       var re = new RegExp("([?|&])" + key + "=.*?(&|$)", "i");
       separator = uri.indexOf('?') !== -1 ? "&" : "?";
@@ -101,8 +144,10 @@
       }
       return url;
     }
+
     $(document).ready(function() {
-      $('button[data-viewport-width]').on('click', function(e) {
+
+      $('body').on('click', 'button[data-viewport-width]', function(e) {
         if($(this).attr('data-viewport-width') == '100%') {
           newWidth = '100%';
         }else{
@@ -115,13 +160,15 @@
         }
         $('button[data-viewport-width]').removeClass('active');
         $(this).addClass('active');
+        setCookie('device', $(this).attr('data-device'));
         // $('#resizerFrame').stop().animate({'max-width': newWidth, 'max-height': newHeight}, 200);
         $('#resizerFrame').css({'max-width': newWidth, 'max-height': newHeight});
         e.preventDefault();
         return false;
       });
-      $('button.rotate').on('click', function(e) {
-        $(this).toggleClass('landscape');
+
+      $('body').on('click', 'button.rotate', function(e) {
+        // $(this).toggleClass('landscape');
         $('button[data-rotate=true]').each(function() {
           $(this).toggleClass('landscape');
           width = $(this).attr('data-viewport-width');
@@ -131,10 +178,21 @@
           $(this).attr('data-viewport-height', width);
           if($(this).hasClass('active')) {
             $(this).trigger('click');
+            if( $(this).hasClass('landscape') ) {
+              setCookie('orientation', 'landscape');
+            }else{
+              eraseCookie('orientation');
+            }
           }
         });
       });
-      $('#closeResizer').on('click', function(e) {
+
+      $('body').on('click', 'button.refresh', function(e) {
+        $(this).find('i[class*="icon-"]').addClass('icon-rotate-360');
+        document.getElementById('resizerFrame').contentWindow.location.reload();
+      });
+
+      $('body').on('click', '#closeResizer', function(e) {
         newWidth = $(window).width();
         newHeight = $(window).height();
         $('button[data-viewport-width]').removeClass('active');
@@ -146,11 +204,25 @@
         e.preventDefault();
         return false;
       });
+
       $('#resizerFrame').on('load', function() {
         var url = basename(this.contentWindow.location.href);
         var stateObj = { resizer: 'resizer' };
         history.pushState(stateObj, url, href);
       });
+
+      if( readCookie('device') ) {
+        $('[data-device="'+readCookie('device')+'"]').trigger('click');
+      }
+      if( readCookie('orientation') == 'landscape' ) {
+        $('.rotate').trigger('click');
+      }
+
+      var iframe = document.getElementById('resizerFrame');
+      var origin = window.location.protocol + '//' + window.location.host;
+      if( origin =='<?php echo $root_url ?>' ) {
+
+      }
     });
   </script>
 <?php
@@ -194,15 +266,28 @@ if(!isset($_REQUEST['url']) || empty($_REQUEST['url'])) {
 ?>
   <div id="resizer" class="bounceInDown animated">
     <ul class="button-group">
-      <li><button class="charcoal desktop-only" title="Fullscreen" data-viewport-width="100%" data-viewport-height="100%"><i class="icon-fullscreen"></i></button></li>
-      <li><button class="charcoal desktop-only" title="Desktop" data-viewport-width="1920px" data-viewport-height="1080px"><i class="icon-desktop"></i></button></li>
-      <li><button class="charcoal desktop-only" title="13 &#39; MacBook Pro" data-viewport-width="1280px" data-viewport-height="800px"><i class="icon-laptop"></i></button></li>
-      <li><button class="charcoal hide-on-small-tablet hide-on-mobile" title="iPad" data-rotate="true" data-viewport-width="768px" data-viewport-height="1024px"><i class="icon-tablet"></i></button></li>
-      <li><button class="charcoal hide-on-mobile" title="Android Tablet" data-rotate="true" data-viewport-width="720px" data-viewport-height="1280px"><i class="android icon-tablet"></i></button></li>
-      <li><button class="charcoal" title="Android Phone" data-rotate="true" data-viewport-width="480px" data-viewport-height="720px"><i class="android icon-mobile-phone"></i></button></li>
-      <li><button class="charcoal" title="iPhone" data-rotate="true" data-viewport-width="320px" data-viewport-height="640px"><i class="icon-mobile-phone"></i></button></li>
-      <li><button class="charcoal rotate" title="Toggle Landscape/Portrait"><i class="icon-refresh"></i></button></li>
+<?php
+if($iframe_access) {
+?>
+      <li><button class="charcoal refresh" title="Refresh Page"><i class="icon-refresh"></i></button></li>
+<?php
+}
+?>
+      <li><button class="charcoal desktop-only" title="Fullscreen" data-device="fullscreen" data-viewport-width="100%" data-viewport-height="100%"><i class="icon-fullscreen"></i></button></li>
+      <li><button class="charcoal desktop-only" title="Desktop" data-device="desktop" data-viewport-width="1920px" data-viewport-height="1080px"><i class="icon-desktop"></i></button></li>
+      <li><button class="charcoal desktop-only" title="13 &#39; MacBook Pro" data-device="macbook" data-viewport-width="1280px" data-viewport-height="800px"><i class="icon-laptop"></i></button></li>
+      <li><button class="charcoal hide-on-small-tablet hide-on-mobile" data-device="ipad" title="iPad" data-rotate="true" data-viewport-width="768px" data-viewport-height="1024px"><i class="icon-tablet"></i></button></li>
+      <li><button class="charcoal hide-on-mobile" title="Android Tablet" data-device="tablet" data-rotate="true" data-viewport-width="720px" data-viewport-height="1280px"><i class="android icon-tablet"></i></button></li>
+      <li><button class="charcoal" title="Android Phone" data-rotate="true" data-device="android" data-viewport-width="480px" data-viewport-height="720px"><i class="android icon-mobile-phone"></i></button></li>
+      <li><button class="charcoal" title="iPhone" data-rotate="true" data-device="iphone" data-viewport-width="320px" data-viewport-height="640px"><i class="icon-mobile-phone"></i></button></li>
+      <li><button class="charcoal rotate" data-rotate="true" title="Toggle Landscape/Portrait"><i class="icon-rotate-left"></i></button></li>
+<?php
+if($iframe_access) {
+?>
       <li><button class="charcoal" id="closeResizer"><i class="icon-remove"></i></button></li>
+<?php
+}
+?>
     </ul>
   </div>
   <iframe id="resizerFrame" src="<?php echo $url ?>" frameborder="0"></iframe>
